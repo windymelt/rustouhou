33246a9acc1a998714db00009d6a511176958874
Windymelt <windymelt@3qe.us>
2022-03-26 22:33:29 +0900

    fundamental web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, sync::Arc};
+
+use actix_cors::Cors;
+use actix_web::{
+    get, middleware, route,
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder,
+};
+use actix_web_lab::respond::Html;
+
+#[get("/")]
+async fn default_endpoint() -> impl Responder {
+    Html("<h1>Hello, world!!!</h1>".to_string())
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    log::info!("Starting server");
+    HttpServer::new(move || {
+        App::new()
+            .service(default_endpoint)
+            .wrap(Cors::permissive())
+            .wrap(middleware::Logger::default())
+    })
+    .workers(2)
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
